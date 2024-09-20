e31cee8899d491e6895c521b25b20c49b6f70003
Dean Peterson <peterson.dean@gmail.com>
2024-09-20 11:14:41 -0500

    fix keycloak.ts

--- react-frontend/src/keycloak.ts
+++ react-frontend/src/keycloak.ts
@@ -2,7 +2,7 @@
 import Keycloak from 'keycloak-js';
 
 const keycloak = Keycloak({
-  "url": process.env.REACT_APP_KEYCLOAK_URL;, // Replace with your Keycloak server URL
+  "url": process.env.REACT_APP_KEYCLOAK_URL, // Replace with your Keycloak server URL
   "realm": 'fihr-rag-llm',
   "clientId": 'fihr-rag-chat'
 });
