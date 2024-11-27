255959820a7bc2d5b3b6485a7f9be0a8766bda56
Dean Peterson <peterson.dean@gmail.com>
2024-11-27 12:50:30 -0600

    fixed remove sql tags

--- vector-ask/src/main/java/com/enterprisewebservice/database/SqlUtil.java
+++ vector-ask/src/main/java/com/enterprisewebservice/database/SqlUtil.java
@@ -2,7 +2,8 @@ package com.enterprisewebservice.database;
 
 public class SqlUtil {
     public static String stripSqlTags(String input) {
-        return input.replaceAll("^<sql>(.*?)</sql>$", "$1");
+        if (input == null) return null;
+        return input.replace("<sql>", "").replace("</sql>", "");
     }
     
 }
